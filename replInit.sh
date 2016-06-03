005ab9d03fcb890d0beb2d47b799b835b964e69b
Daniel Phillips <danphi@gmail.com>
2016-06-03 12:00:12 +0700

    fixes

--- replInit.sh
+++ replInit.sh
@@ -5,33 +5,32 @@ if [ -f /data/db/.initDone ]; then
   exit 0
 fi
 
-# set a default oplog size if not yet defined
-availDiskSpace=$(df -k / | tail -1 | awk '{print $4}')
-OPLOG_SIZE=$(($availDiskSpace*5/100))
+echo "--- Starting MongoDB setup. ---"
 
-echo "Creating replica set rs0 with oplog size ${OPLOG_SIZE}"
-
-mongod --replSet rs0 --oplogSize ${OPLOG_SIZE} --directoryperdb --storageEngine wiredTiger &
+mongod --replSet rs0 --oplogSize 1073741824 --directoryperdb --storageEngine wiredTiger &
 
 # wait for mongod to start
 RET=1
-printf "Waiting for mongod to start..."
+printf "--- Waiting for mongod to start..."
 while [[ RET -ne 0 ]]; do
   printf "."
   sleep 5
   mongostat -n 1 >/dev/null 2>&1
   RET=$?
 done
-printf "\n"
+echo " "
 
+echo "--- Running rs.initiate() ---"
 mongo admin --eval "rs.initiate()"
 sleep 5
+echo "--- Running rs.conf() ---"
 mongo admin --eval "rs.conf()"
 sleep 5
+echo "--- Running db.shutdownServer({force: 1}); ---"
 mongo admin --eval "db.shutdownServer({force: 1});"
 
 # make sure this script is only run once
 touch /data/db/.initDone
 
-echo "MONGO_OPLOG_URL=mongodb://<address>/local"
-echo "Finished MongoDB setup."
+echo "--- MONGO_OPLOG_URL=mongodb://<address>/local"
+echo "--- Finished MongoDB setup. ---"
