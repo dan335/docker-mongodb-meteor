21a71f8f5c7f512c4f3841583796d7b31a940c8f
Daniel Phillips <danphi@gmail.com>
2016-06-03 12:22:59 +0700

    oplogger

--- replInit.sh
+++ replInit.sh
@@ -11,7 +11,7 @@ mongod --replSet rs0 --oplogSize 1073741824 --directoryperdb --storageEngine wir
 
 # wait for mongod to start
 RET=1
-printf "--- Waiting for mongod to start..."
+printf "Waiting for mongod to start..."
 while [[ RET -ne 0 ]]; do
   printf "."
   sleep 5
@@ -26,11 +26,14 @@ sleep 5
 echo "--- Running rs.conf() ---"
 mongo admin --eval "rs.conf()"
 sleep 5
+echo "--- Creating oplog user ---"
+mongo admin --eval "db.createUser({ user:'oplogger', pwd:'oplogger', roles:[{rold:'read', db:'local'}] });"
+sleep 5
 echo "--- Running db.shutdownServer({force: 1}); ---"
 mongo admin --eval "db.shutdownServer({force: 1});"
 
 # make sure this script is only run once
 touch /data/db/.initDone
 
-echo "--- MONGO_OPLOG_URL=mongodb://<address>/local"
+echo "--- MONGO_OPLOG_URL=mongodb://oplogger:oplogger@<address>/local?authSource=admin"
 echo "--- Finished MongoDB setup. ---"
