e340c9c7b56bc6b4f62de57ae5c5f204cd99d61b
Zadkiel <aslafy-z@users.noreply.github.com>
2017-03-16 19:30:43 +0000

    Update repl_init.sh

--- repl_init.sh
+++ repl_init.sh
@@ -12,11 +12,11 @@ mongod --replSet rs0 &
 # wait for mongod to start
 while ! mongo --eval "db.version()" > /dev/null 2>&1; do sleep 0.1; done
 
-mongo admin --eval "rs.initiate()"
+mongo local --eval "rs.initiate()"
 sleep 5
-mongo admin --eval "rs.conf()"
+mongo local --eval "rs.conf()"
 sleep 5
-mongo admin --eval "db.shutdownServer({force: 1});"
+mongo local --eval "db.shutdownServer({force: 1});"
 
 # make sure this script is only run once
 touch /data/db/.init_done
