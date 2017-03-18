ce2bd644ec513e2418adafb664ea6d1175a7a293
Zadkiel <aslafy-z@users.noreply.github.com>
2017-03-18 16:52:10 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ sh /root/repl_init.sh || exit $?
 # Make sure that database is owned by user mongodb
 [ "$(stat -c %U /data/db)" = mongodb ] || chown -R mongodb /data/db
 
-exec su -s /bin/sh -c "exec mongod" mongodb
+exec su -s /bin/sh -c "exec mongod --replSet rs0" mongodb
