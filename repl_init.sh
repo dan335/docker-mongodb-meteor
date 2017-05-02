a62685f29c5ffac8c44b16dee2fb48fc2febd66b
Zadkiel <aslafy-z@users.noreply.github.com>
2017-05-02 17:49:09 +0100

    Update repl_init.sh

--- repl_init.sh
+++ repl_init.sh
@@ -21,7 +21,6 @@ mongo admin --eval "db.shutdownServer({force: 1});"
 
 echo "[x] Waiting for MongoDB to stop"
 while mongo --eval "db.version()" > /dev/null 2>&1; do sleep 0.1; done
-kill -2 %1
 
 # make sure this script is only run once
 touch /data/db/.init_done
